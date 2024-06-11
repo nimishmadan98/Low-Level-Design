3c86696c249a5f65136927ea7519907de619e953
nimishmadan98 <nimishmadan98@gmail.com>
2024-06-11 22:45:46 +0530

    add approach

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -15,8 +15,21 @@ Player
 PeiceO
 PeiceX
 
+Initialize Game
+---------------
 
+    Initialize Board with Size
+    Initialize Players with Name and PeiceType
+    Add Players to List
+Start Game
+----------
 
+    Continue Game till no one is winner or turn are completed
+    Print Board at each step, Enque and Deque Players to maintain turns
+    Take input from players and add input to board
+    Check winner at each step
+    print tie if there is no winner
+    
 */
 
 
